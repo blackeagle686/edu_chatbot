3ea54e10b7896694534323a21ef7fd320dfb4dbb
blackeagle686 <mathematecs1@gmail.com>
2026-04-19 14:06:14 +0200

    Auto-commit: 2026-04-19 14:06:14

--- auto_push.sh
+++ auto_push.sh
@@ -8,7 +8,7 @@ echo "Press Ctrl+C to stop."
 
 while true; do
     # Check if there are any uncommitted changes
-    if [[ -n $(git status -s) ]]; then
+    if [[ -n "$(git status --porcelain)" ]]; then
         echo "Changes detected! Committing and pushing..."
         
         # Add all changes
